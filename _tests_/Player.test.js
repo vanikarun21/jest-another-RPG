7c8d4e6295044a983d728f6ffa389a12a221ba21
vanikarun21 <vanikarun21@yahoo.com>
2022-06-18 15:19:05 -0500

    updated player js and test files

--- _tests_/Player.test.js
+++ _tests_/Player.test.js
@@ -1,3 +1,9 @@
+const Potion = require('../lib/Potion');
+
+jest.mock('../lib/Potion.js');
+
+console.log(new Potion());
+
 const Player = require('../lib/Player');
 
 test('creates a player object',() => {
@@ -7,4 +13,7 @@ test('creates a player object',() => {
     expect(player.health).toEqual(expect.any(Number));
     expect(player.strength).toEqual(expect.any(Number));
     expect(player.agility).toEqual(expect.any(Number));
+    expect(player.inventory).toEqual(
+        expect.arrayContaining([expect.any(Object)])
+      );
 });
